9deb0441f5b84b7a0b8088b563a8064cf220d58c
Anu Jose <anujose1989@gmail.com>
2017-10-05 06:07:22 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,53 @@
+var a = 10
+var b = 5
+
+function add(a,b) {
+  return a+b;
+}
+function subtract(a,b) {
+  return a-b;
+}
+
+function divide(a,b) {
+  return a/b;
+}
+function multiply(a,b) {
+  return a*b;
+}
+function inc(a){
+  return a+1;
+}
+function dec(a){
+  return a-1;
+}
+console.log(add(a,b))
+console.log(subtract(a,b))
+console.log(divide(a,b))
+console.log(multiply(a,b))
+console.log(inc(a))
+console.log(dec(a));
+
+function makeInt(a){
+  return parseInt(a);
+}
+makeInt("10");
+
+function makeInt(a,base){
+  return parseInt(a,base);
+}
+makeInt('0x2328',10);
+
+function makeInt(a){
+  return parseInt(a);
+}
+makeInt("sldkjflksjf");
+
+function preserveDecimal(a){
+  return parseFloat(a);
+}
+preserveDecimal("2.222");
+
+function makeIpreserveDecimalnt(a){
+  return parseFloat(a);
+}
+makeInt("sldkjflksjf");
